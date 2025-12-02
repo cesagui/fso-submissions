1dab61dc229e446d1518ed9a796399ab97b3e102
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-12-02 14:55:49 -0800

    exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,19 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+
+morgan.token('type', function (req, res) { 
+    return JSON.stringify(req.body)
+}
+)
+
+// app.use(morgan(':name'))
+
+// morgan.token('name', function getID (req) {
+//     return req
+// })
 
 let persons = [
     { 
