bcd166c69d3a5d652426410745badbd8b17d5187
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-12-02 12:53:00 -0800

    exercises 3.1-2

---
+++ part3/phonebook/index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/phonebook', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    const size = Object.keys(persons).length
+    const timeStamp = Date.now()
+    response.send(`<p>Phonebook has info for ${size} people</p><p>${new Date(timeStamp)}</p>`)
+    //response.send(``)
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
