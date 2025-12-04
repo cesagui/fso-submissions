3f355a651326a3a4a3620aa843d668284c2ae009
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-12-03 18:46:23 -0800

    exercise 3.9-10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
+app.use(cors())
 
 morgan.token('type', function (req, res) { 
     return JSON.stringify(req.body)
@@ -110,7 +113,7 @@ app.delete('/api/phonebook/:id',  (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
