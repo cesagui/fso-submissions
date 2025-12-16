2c80458e8ca53491b2229d75324cfd656a556e47
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-12-16 13:30:14 -0800

    fix front-end not loading from server

--- part2/phonebook/src/components/persons.jsx
+++ part2/phonebook/src/components/persons.jsx
@@ -1,12 +1,12 @@
 const Persons = ({persons, filter, handleDelete}) => {
-    const filteredPersons = persons.filter(person =>
-        person.name.toLowerCase().startsWith(filter.toLowerCase())
-    )
-    return (
-        <div>
-            {filteredPersons.map(person => <PersonEntry key = {person.id} person = {person} onClick = {() => {handleDelete(person.id)}}/>)}
-        </div>   
-    )
+  const filteredPersons = persons.filter(person =>
+      person.name.toLowerCase().startsWith(filter.toLowerCase())
+  )
+  return (
+      <div>
+          {filteredPersons.map(person => <PersonEntry key = {person.id} person = {person} onClick = {() => {handleDelete(person.id)}}/>)}
+      </div>   
+  )
 }   
 
 const PersonEntry = ({person, onClick}) => {
