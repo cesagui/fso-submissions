be5fdae23d732a617c89c89c2a00e0d5819858bd
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-12-21 17:28:32 -0800

    add favoriteBlog, topBlogger and tests for ch 4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,3 +1,5 @@
+var lodash = require('lodash');
+
 const dummy = (blogs) => {
     return 1
 }
@@ -12,7 +14,27 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 } 
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0){
+        return {}
+    }
+
+    const reducer = (prev, current) => {
+        return (prev.likes > current.likes) ? prev : current;
+    }
+
+    return blogs.reduce(reducer, blogs[0])
+}
+
+const mostBlogs = (blogs) => {
+    const byBlogCount = lodash.countBy(blogs, 'author')
+
+    return Object.keys(byBlogCount).reduce((a,b) => byBlogCount[a] > byBlogCount[b] ? a : b)
+}
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
