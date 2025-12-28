f709dee3a89ac0a34c18b66e34adfb637abb2b5b
Cesar Aguirre <cesar.aguirre.work@gmail.com>
2025-12-27 22:22:03 -0800

    add dropdown menu to blog entries

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,53 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, useEffect } from 'react'
+import Toggleable from './Toggleable'
+import UserService from '../services/users'
+
+const Blog = ({ blog }) => {
+    const [user, setUser] = useState(null) // used to store the user associated with user
+    const [visible, setVisible] = useState(false) // related to visibility
+
+    
+    useEffect(() => { // used to fetch the user data upon first render
+        UserService.getUser(blog.user.id)
+            .then(response => setUser(response.data))
+    }, [blog.user.id]) // effect is ran whenever blog.user.id changes
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    const handleLike = () => {
+        console.log('like!')
+    }
+
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+    const hideWhenVisible = {display : visible ? 'none' : ''}
+    const showWhenVisible = {display : visible ? '' : 'none'}
+
+    if (!user) {
+        return null
+    }
+
+    return (
+        <div style = {blogStyle} >
+            {blog.title} {blog.author}  
+            <span>
+                <button style = {hideWhenVisible} onClick = {toggleVisibility}>show</button>
+                <button style = {showWhenVisible} onClick = {toggleVisibility}>hide</button>
+            </span>
+            <div style = {showWhenVisible}>
+                <p>{blog.url}</p>
+                <p>likes {blog.likes} <button onClick = {handleLike}>like</button></p>
+                <p>{user.name}</p>
+            </div>
+        </div> 
+    )
+} 
 
 export default Blog
---
+++ part5/bloglist-frontend/src/services/users.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+const baseUrl = '/api/users'
+
+const getUser = async (id) => {
+    let user = await axios(`${baseUrl}/${id}`)
+    return user
+}
+
+export default {
+    getUser
+}
